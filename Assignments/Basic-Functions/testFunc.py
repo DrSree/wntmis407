8a37531684d9e9357a320c818ad015abc16812f3
Joe Wanat <wanat.joe@gmail.com>
2014-09-15 15:09:40 -0500

    added fodler

---
+++ Assignments/Basic-Functions/testFunc.py
@@ -0,0 +1,10 @@
+"""
+getValue() function
+Reads an integer input from console and assigns it to a local variable
+"""
+
+def getValue():
+    given_int = raw_input("Please provide an integer: ")
+    return given_int
+
+
